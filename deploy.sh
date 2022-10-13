7ee0af9f1d9538a8e68b57524bbdbecf0b0ff33c
Mateusz Wykurz <wykurz@gmail.com>
2022-10-13 11:49:16 -0600

    Add ARMv7 compilation target

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ set -o xtrace
 
 readonly TARGET_HOST=192.168.137.245
 readonly TARGET_PATH=/home/mateusz/rust-led-lights
-readonly SOURCE_PATH=./target/release/rust-led-lights
+readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
+readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/rust-led-lights
 
-cargo build --release
+cargo build --release --target=${TARGET_ARCH}
 rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
 ssh -t ${TARGET_HOST} ${TARGET_PATH}
