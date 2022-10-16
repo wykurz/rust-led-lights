bc677945cc6f5b9fc7cfc6562c8325c86b16967c
Mateusz Wykurz <wykurz@gmail.com>
2022-10-15 20:47:12 -0600

    Make the nix shell package the libs as well

--- default.nix
+++ default.nix
@@ -1,4 +1,5 @@
 let
+  arm = import <nixpkgs> { crossSystem.config = "armv7l-unknown-linux-musleabihf"; };
   rust_overlay = import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz);
   unstable = import <nixos-unstable-small> { };
   nixpkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
@@ -12,8 +13,13 @@ in
   stdenv.mkDerivation {
     name = "rust-shell";
     buildInputs = [
+      arm.stdenv.cc
       unstable.rust-analyzer  # must be before rust so the PATH picks this one first
       myrust
     ];
     RUST_SRC_PATH = "${myrust}/lib/rustlib/src/rust/src";
+    shellHook =
+    ''
+      export LD_LIBRARY_PATH=${arm.binutils-unwrapped}/lib
+    '';
   }
