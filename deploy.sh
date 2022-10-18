e7144ccde8b5cdb52363818d4d8feb7c6dc1f2e2
Mateusz Wykurz <wykurz@gmail.com>
2022-10-17 19:15:46 -0600

    Use another example for my lights

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-readonly TARGET_HOST=192.168.137.137
+readonly TARGET_HOST=192.168.0.29
 readonly TARGET_PATH=/home/mateusz/rust-led-lights
 readonly TARGET_ARCH=armv7-unknown-linux-musleabihf
 readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/rust-led-lights
